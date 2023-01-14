6f424242c39ac4e957c4d3811360516e8e23a087
Sakshi Pore <88977771+sakshipore@users.noreply.github.com>
2023-01-14 18:45:55 +0530

    Sakshi dev (#5)
    * Firebase CLI setup
    
    * GetX setup

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,1 +1,14 @@
 // Add routes used in application
+
+import 'package:get/get.dart';
+import 'package:nscc_app/router/routes_names.dart';
+import 'package:nscc_app/view/home_screen.dart';
+
+class AppRoutes {
+  static final routes = [
+    GetPage(
+      name: RoutesNames.homeScreen,
+      page: () => HomeScreen(),
+    ),
+  ];
+}
