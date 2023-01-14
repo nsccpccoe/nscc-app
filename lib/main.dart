e645598f603b4cb974dee55fef7d25fb7eea5c3c
sakshipore <88977771+sakshipore@users.noreply.github.com>
2023-01-14 12:32:46 +0530

    Firebase CLI setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:nscc_app/firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
